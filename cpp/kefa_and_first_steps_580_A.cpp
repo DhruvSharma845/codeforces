ce85b9c9074d992592690d46549bc2f730f642ec
dhruvsharma <dhruv.sharma@accoliteindia.com>
2019-11-03 19:56:53 +0530

    Adding solution - kefa and first steps

---
+++ cpp/kefa_and_first_steps_580_A.cpp
@@ -0,0 +1,51 @@
+//
+// Created by dhruv on 03-11-2019.
+//
+
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+class LongestIncreasingSubSegment {
+private:
+    int* arr;
+    int size;
+public:
+    LongestIncreasingSubSegment(int* _arr, int _size): arr(_arr), size(_size) {}
+
+    int findLengthOfLongestIncreasingSubSegment();
+};
+
+int LongestIncreasingSubSegment::findLengthOfLongestIncreasingSubSegment() {
+    int currentMax = numeric_limits<int>::min();
+    int count = 1;
+    for (int i = 1; i < this->size; ++i) {
+        if(this->arr[i] >= this->arr[i-1]) {
+            count++;
+            if(count > currentMax) {
+                currentMax = count;
+            }
+        }
+        else {
+            count = 1;
+        }
+    }
+    return max(currentMax, count);
+}
+
+int main() {
+    int N;
+    cin>>N;
+
+    int* arr = new int[N];
+    for (int i = 0; i < N; ++i) {
+        cin>>arr[i];
+    }
+
+    LongestIncreasingSubSegment* lis = new LongestIncreasingSubSegment(arr, N);
+    cout<<lis->findLengthOfLongestIncreasingSubSegment();
+
+    return 0;
+}
+
