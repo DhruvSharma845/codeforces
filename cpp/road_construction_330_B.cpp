6d364ec1fe7f9aab07475cb48376163adb0095c1
DhruvSharma845 <dhruvsharma845@gmail.com>
2021-08-16 12:00:47 +0530

    Adding solution for road construction

---
+++ cpp/road_construction_330_B.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <utility>
+
+auto main() -> int {
+    int N, M;
+    std::cin >> N >> M;
+
+    std::unordered_set<int> bannedVertices;
+    for (int i = 0; i < M; i++) {
+        int f,s;
+        std::cin >> f >> s;
+        bannedVertices.insert(f);
+        bannedVertices.insert(s);
+    }
+    int root = -1;
+    for(int i = 1; i <= N; i++) {
+        if(bannedVertices.find(i) == bannedVertices.end()) {
+            root = i;
+            break;
+        }
+    }
+
+    std::cout << (N-1) << std::endl;
+    for (int i = 1; i <= N; i++) {
+        if(i != root) {
+            std::cout << root << " " << i << std::endl;
+        }
+    }
+}
